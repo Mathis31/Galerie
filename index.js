b072862198006d166e20f712a4870ed6f3b81dad
Mathis31 <52449752+Mathis31@users.noreply.github.com>
2020-11-05 13:46:31 +0100

    commit
    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var container = document.getElementById('container');
 
-function loadDistantFile(url) {
+function loadJsonFile(url) {
     return new Promise(function (resolve, reject) {
         $.getJSON(url)
             .done(function (data) {
@@ -13,40 +13,40 @@ function loadDistantFile(url) {
     );
 }
 
-loadDistantFile("https://compassionate-lichterman-736604.netlify.app/index.json").then(res => console.log(res));
+function createCard(i){
 
-/*function createCard(i){
-    
-    let div = document.createElement("div");
-    div.className += "col-12 col-sm-6 col-lg-4 col-xl-3";
+    loadDistantFile("https://compassionate-lichterman-736604.netlify.app/index.json").then(res => {
+        let div = document.createElement("div");
+        div.className += "col-12 col-sm-6 col-lg-4 col-xl-3";
 
-    let card = document.createElement("div");
-    card.className += "card mb-4";
+        let card = document.createElement("div");
+        card.className += "card mb-4";
 
-    let img = document.createElement("img");
-    img.setAttribute("src", "https://via.placeholder.com/500x280.png");
-    img.setAttribute("width", "500");
-    img.setAttribute("height", "280");
-    img.className += "card-img-top img-fluid";
-    img.setAttribute("alt", "placeholder");
-    card.append(img);
+        let img = document.createElement("img");
+        img.setAttribute("src", res.ig);
+        img.setAttribute("width", "500");
+        img.setAttribute("height", "280");
+        img.className += "card-img-top img-fluid";
+        img.setAttribute("alt", "placeholder");
+        card.append(img);
 
-    let divBody = document.createElement("div");
-    divBody.className += "card-body";
-    card.append(divBody);
+        let divBody = document.createElement("div");
+        divBody.className += "card-body";
+        card.append(divBody);
 
-    let p = document.createElement('p');
-    p.className += "card-text p-3";
-    p.textContent = text[i].substring(0,100);
-    card.append(p);
+        let p = document.createElement('p');
+        p.className += "card-text p-3";
+        p.textContent = res.text[i].substring(0,100);
+        card.append(p);
 
-    div.append(card);
+        div.append(card);
 
-    return div;
+        return div;
+    });
 
 }
 
 for(let i = 0; i<6; i++){
     let div = createCard(i);
     container.append(div);
-}*/
+}
