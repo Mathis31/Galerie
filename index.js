31c3294475e8fe4f582ebea56ec3672a8a5d4593
Mathis31 <52449752+Mathis31@users.noreply.github.com>
2020-11-05 13:57:46 +0100

    commit
    commit

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-var container = document.getElementById('container');
-
 function loadJsonFile(url) {
     return new Promise(function (resolve, reject) {
         $.getJSON(url)
             .done(function (data) {
-                resolve.json(data));
+                resolve(data);
             })
             .fail(function () {
                 reject(Error('Impossible d\'ouvrir le fichier ' + url));
@@ -13,17 +11,15 @@ function loadJsonFile(url) {
     );
 }
 
-async function get(){
+Promise.all(loadJsonFile("https://compassionate-lichterman-736604.netlify.app/index.json")).then(datas => {
+    var container = document.getElementById('container');
+    for(let i = 0; i<6; i++){
+        let div = createCard(datas);
+        container.append(div);
+    }
+})
 
-    let toto = await loadJsonFile("https://compassionate-lichterman-736604.netlify.app/index.json");
-
-    return toto;
-}
-
-
-console.log(get());
-
-/*function createCard(i){
+function createCard(datas){
     
     let div = document.createElement("div");
     div.className += "col-12 col-sm-6 col-lg-4 col-xl-3";
@@ -32,7 +28,7 @@ console.log(get());
     card.className += "card mb-4";
 
     let img = document.createElement("img");
-    img.setAttribute("src", "https://via.placeholder.com/500x280.png");
+    img.setAttribute("src", datas.img);
     img.setAttribute("width", "500");
     img.setAttribute("height", "280");
     img.className += "card-img-top img-fluid";
@@ -45,7 +41,7 @@ console.log(get());
 
     let p = document.createElement('p');
     p.className += "card-text p-3";
-    p.textContent = text[i].substring(0,100);
+    p.textContent = datas.text[i].substring(0,100);
     card.append(p);
 
     div.append(card);
@@ -53,8 +49,3 @@ console.log(get());
     return div;
 
 }
-
-for(let i = 0; i<6; i++){
-    let div = createCard(i);
-    container.append(div);
-}*/
