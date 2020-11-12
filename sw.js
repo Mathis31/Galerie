ade271650f9d71d347744ea779476e7354f4e8e1
Mathis31 <52449752+Mathis31@users.noreply.github.com>
2020-11-12 14:42:46 +0100

    commit
    commit

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ self.addEventListener('fetch', (e) => {
     console.log('[Service Worker] Ressource récupérée ' + e.request.url);
     if(e.request.url === "https://compassionate-lichterman-736604.netlify.app/GalerieRepos/index.json"){
         e.respondWith(
-            caches.match(e.request).catch(async function() {
-                return fetch(e.request).then(res => {return [{"url":"https://via.placeholder.com/500x280.png",
+            caches.match(e.request.url).catch(async function() {
+                return fetch(e.request.url).then(res => {return [{"url":"https://via.placeholder.com/500x280.png",
                 "description":"Photo",
                 "author":"Anonyme",
                 "updated":"2020-10-10",
