11609a0034df4a3feb0f2d31edaed09711673b74
Mathis31 <52449752+Mathis31@users.noreply.github.com>
2020-11-12 14:22:54 +0100

    commit
    commit

--- sw.js
+++ sw.js
@@ -2,6 +2,6 @@ self.addEventListener('fetch', (e) => {
     console.log('[Service Worker] Ressource récupérée ' + e.request.url);
 });
 
-self.addEventListener('parseJson'), (e) => {
+self.addEventListener('parseJson', (e) => {
     return 'toto';
-}
+});
