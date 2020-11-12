ee8b6035d7d40533a1660309accadbacefbf02b8
Mathis31 <52449752+Mathis31@users.noreply.github.com>
2020-11-12 15:25:10 +0100

    commit
    commit

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function createCard(element){
 window.addEventListener("DOMContentLoaded", (event) => {
     fetch("https://compassionate-lichterman-736604.netlify.app/GalerieRepos/index.json")
     .then(res => res.json())
-    .then(datas =>
+    .then((datas) =>
     {
         var container = document.getElementById('container');
         datas.forEach(element => {
