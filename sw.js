73d8d657b4076f16c26c17461259e6458a31b806
Mathis31 <52449752+Mathis31@users.noreply.github.com>
2020-11-12 15:44:52 +0100

    commit
    commit

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
 self.addEventListener('fetch', (e) => {
     console.log('[Service Worker] Ressource récupérée ' + e.request.url);
+    if(e.request.url === "https://compassionate-lichterman-736604.netlify.app/GalerieRepos/index.json"){
+        e.response.width(
+            fetch(e.request).then((response) => {
+                return response.json().then((json) => {
+                    return new Response(JSON.stringify([{
+                        "url":"https://via.placeholder.com/500x280.png",
+                        "description":"Photo",
+                        "author":"Anonyme",
+                        "updated":"2020-10-10",
+                        "created":"2020-10-10"
+                    }]));
+                })
+            })
+        )
+    }
 });
