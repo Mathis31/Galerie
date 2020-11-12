a853be87644334d1d4204005acc029f34a34c229
Mathis31 <52449752+Mathis31@users.noreply.github.com>
2020-11-12 14:47:34 +0100

    commit
    commit

--- sw.js
+++ sw.js
@@ -2,22 +2,7 @@ self.addEventListener('fetch', (e) => {
     console.log('[Service Worker] Ressource récupérée ' + e.request.url);
     if(e.request.url === "https://compassionate-lichterman-736604.netlify.app/GalerieRepos/index.json"){
         e.respondWith(
-            // Ici je crée une réponse à partir
-            // de rien qui contient uniquement
-            // "Hello Toto"
-            new Response(
-              new Blob(
-                ["Hello Toto"],
-                {type : "text/html"}
-              ),
-              {
-                status: 200,
-                statusText: "OK",
-                headers: {
-                  "Content-Type": "text/html",
-                }
-              }
-            )
-          );
+            ['toto']
+        )
     }
 });
