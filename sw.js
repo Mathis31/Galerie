e61b5921e2b0f354d2eb53675a3dbf2e41624bd2
Mathis31 <52449752+Mathis31@users.noreply.github.com>
2020-11-12 14:52:44 +0100

    commit
    commit

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-var myBody = new Blob([
+var myBody = [
     {
         "url":"https://via.placeholder.com/500x280.png",
         "description":"Photo",
         "author":"Anonyme",
         "updated":"2020-10-10",
         "created":"2020-10-10"
-    }]);
+    }];
 
 self.addEventListener('fetch', (e) => {
     console.log('[Service Worker] Ressource récupérée ' + e.request.url);
