27c7cbcd4e4f94e7d16060fe951ba54c581b2596
Mathis31 <52449752+Mathis31@users.noreply.github.com>
2020-11-19 15:25:29 +0100

    commit
    commit

--- index.js
+++ index.js
@@ -1,157 +1,155 @@
 // Set service worker
 
-if('serviceWorker' in navigator){
-
-    navigator.serviceWorker.register("/sw.js")
-    .then((reg) => {
-        console.log("Votre service worker a été enregistré!");
-    })
-    .catch((error) => {
-        console.error(error);
-    });
-
-} 
-
-else {
-    console.warn("Service workers are not supported.");
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		.register('/sw.js')
+		.then((reg) => {
+			console.log('Votre service worker a été enregistré!');
+		})
+		.catch((error) => {
+			console.error(error);
+		});
+} else {
+	console.warn('Service workers are not supported.');
 }
 
 // For cache
 
-if('cache' in window){
-    caches.open('cacheGalerie')
-    .then( (cache) =>{ 
-        cache.addAll(['/', '/index.html', '/style.css', '/index.js']);
-    })
-    .catch((err)=>{console.log(err)}); 
+if ('cache' in window) {
+	caches
+		.open('cacheGalerie')
+		.then((cache) => {
+			cache.addAll([ '/', '/index.html', '/style.css', '/index.js' ]);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 }
-
 
 // Set badge if offline or not
 
-
 window.addEventListener('offline', (event) => {
-    let divOffline = document.getElementById("divOffline");
-    divOffline.style.visibility = "visible";
+	let divOffline = document.getElementById('divOffline');
+	divOffline.style.visibility = 'visible';
 });
 
 window.addEventListener('online', (event) => {
-    let divOffline = document.getElementById("divOffline");
-    divOffline.style.visibility = "hidden";
+	let divOffline = document.getElementById('divOffline');
+	divOffline.style.visibility = 'hidden';
 });
 
-if(navigator.onLine){
-    let divOffline = document.getElementById("divOffline");
-    divOffline.style.visibility = "hidden";
-}else{
-    let divOffline = document.getElementById("divOffline");
-    divOffline.style.visibility = "visible";
+if (navigator.onLine) {
+	let divOffline = document.getElementById('divOffline');
+	divOffline.style.visibility = 'hidden';
+} else {
+	let divOffline = document.getElementById('divOffline');
+	divOffline.style.visibility = 'visible';
 }
 
-// Create database
+const DB_NAME = 'cardDb';
+const DB_VERSION = 1; 
+ const DB_STORE_NAME = 'cards';
 
-if('indexedDB'in window){
+function openDb() {
+	console.log("openDb ...");
+    var req = indexedDB.open(DB_NAME, DB_VERSION);
+    req.onsuccess = function (evt) {
+      db = this.result;
+      console.log("openDb DONE");
+    };
+    req.onerror = function (evt) {
+      console.error("openDb:", evt.target.errorCode);
+    };
 
-    const ddb = idb.openDB('cardsTable', {autoincrement:true});
+    req.onupgradeneeded = function (evt) {
+        console.log("openDb.onupgradeneeded");
+        var store = evt.currentTarget.result.createObjectStore(
+            DB_STORE_NAME, { keyPath: 'id', autoIncrement: true }
+        );
 
-    const trans = ddb.transaction('objet', 'readwrite');
-    
-    const objet = trans.objectStore('objet');
-    
-    objet.add(
-        {
-            "url":"https://via.placeholder.com/500x280.png",
-            "description":"Photo",
-            "author":"Anonyme",
-            "updated":"2020-10-10",
-            "created":"2020-10-10"
-        }
-    );
-    
-    return trans.complete;
-
-}else{
-    console.log('API not supported');
+      store.createIndex('url', 'url', { unique: true });
+      store.createIndex('description', 'description', { unique: false });
+      store.createIndex('author', 'author', { unique: false });
+      store.createIndex('updated', 'updated', { unique: false });
+      store.createIndex('created', 'created', { unique: false });
+    };
 }
 
-dbPromise.then(function(ddb){
-    const trans= ddb.transaction('objet', 'readwrite');
-    const objet = trans.objectStore('objet');
-    return trans.openCursor()
-})
-.then(function displayItem(cursor){
-    if(!cursor){
-        return;
-    }
-    console.log(cursor.value);
-    cursor.coninue.then(displayItem);
-}); 
+function getObjectStore(store_name, mode) {
+    var tx = db.transaction(store_name, mode);
+    return tx.objectStore(store_name);
+}
+
+function getAll(){
+    var store = getObjectStore(DB_STORE_NAME, 'readwrite');
+    console.log(store.getAll());
+}
 
 // Create card element for images, description, author, updated...
 
+function createCard(element) {
+	let div = document.createElement('div');
+	div.className += 'col-12 col-sm-6 col-lg-4 col-xl-3';
 
-function createCard(element){
+	let card = document.createElement('div');
+	card.className += 'card mb-4';
 
-    let div = document.createElement("div");
-    div.className += "col-12 col-sm-6 col-lg-4 col-xl-3";
+	let img = document.createElement('img');
+	img.setAttribute('src', element.url);
+	img.setAttribute('width', '500');
+	img.setAttribute('height', '280');
+	img.className += 'card-img-top';
+	img.setAttribute('alt', 'placeholder');
+	card.append(img);
 
-    let card = document.createElement("div");
-    card.className += "card mb-4";
+	let divBody = document.createElement('div');
+	divBody.className += 'card-body';
 
-    let img = document.createElement("img");
-    img.setAttribute("src", element.url);
-    img.setAttribute("width", "500");
-    img.setAttribute("height", "280");
-    img.className += "card-img-top";
-    img.setAttribute("alt", "placeholder");
-    card.append(img);
+	let spanAuthor = document.createElement('span');
+	spanAuthor.textContent = 'Author : ' + element.author;
 
-    let divBody = document.createElement("div");
-    divBody.className += "card-body";
+	divBody.append(spanAuthor);
 
-    let spanAuthor = document.createElement('span');
-    spanAuthor.textContent = "Author : " + element.author;
+	let updated = document.createElement('p');
+	updated.textContent = 'Last modified : ' + element.updated;
 
-    divBody.append(spanAuthor);
+	divBody.append(updated);
 
-    let updated = document.createElement('p');
-    updated.textContent = "Last modified : " + element.updated;
+	let description = document.createElement('p');
+	description.className += 'card-text';
+	description.textContent = 'Description : ' + element.description;
 
-    divBody.append(updated);
+	divBody.append(description);
 
-    let description = document.createElement('p');
-    description.className += "card-text";
-    description.textContent = "Description : " + element.description;
+	card.append(divBody);
 
-    divBody.append(description);
+	div.append(card);
 
-    card.append(divBody);
-
-    div.append(card);
-
-    return div;
-
+	return div;
 }
 
 
+// Create database
+
+if ('indexedDB' in window) {
+    openDb();
+    getAll();
+} else {
+	console.log('API not supported');
+}
+
 // Fetch images
 
-
-window.addEventListener("DOMContentLoaded", function() {
-
-    if(navigator.onLine){
-
-        fetch("https://compassionate-lichterman-736604.netlify.app/GalerieRepos/index.json")
-        .then((res) => res.json())
-        .then((datas) =>
-        {
-            var container = document.getElementById('container');
-            datas.forEach(element => {
-                let div = createCard(element);
-                container.append(div);
-            });
-        });
-
-    }
-
+window.addEventListener('DOMContentLoaded', function() {
+	if (navigator.onLine) {
+		fetch('https://compassionate-lichterman-736604.netlify.app/GalerieRepos/index.json')
+			.then((res) => res.json())
+			.then((datas) => {
+				var container = document.getElementById('container');
+				datas.forEach((element) => {
+					let div = createCard(element);
+					container.append(div);
+				});
+			});
+	}
 });
