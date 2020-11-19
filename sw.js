7d9f8b9841abebb61d9ec5ba6690fef0c5d99adc
Mathis31 <52449752+Mathis31@users.noreply.github.com>
2020-11-19 12:09:23 +0100

    commit
    commit

--- sw.js
+++ sw.js
@@ -9,33 +9,31 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
 	console.log('[Service Worker] Ressource récupérée ' + e.request.url);
 	if (navigator.onLine) {
-		if (e.request.url === 'https://compassionate-lichterman-736604.netlify.app/GalerieRepos/index.json') {
-			e.respondWith(
-				fetch(e.request).then(async (response) => {
-                    caches.open('cacheGalerie').then((cache) => {
-                        return cache.add(response);
-                    });
-					return response.json().then((json) => {
-						const jsonFormatted = json.map((j) => ({
-							url: j.url,
-							description: j.description,
-							updated: j.updated,
-							author: j.author
-						}));
-						return new Response(JSON.stringify(jsonFormatted));
-					});
-				})
-			);
-		} else {
-			fetch(e.request).then((response) => {
-				caches.open('cacheGalerie').then((cache) => {
-                    return cache.add(response);
-                });
-			});
-		}
+		caches.open('cacheGalerie').then((cache) => {
+			if (e.request.url === 'https://compassionate-lichterman-736604.netlify.app/GalerieRepos/index.json') {
+				e.respondWith(
+					fetch(e.request).then(async (response) => {
+						cache.add(response);
+						return response.json().then((json) => {
+							const jsonFormatted = json.map((j) => ({
+								url: j.url,
+								description: j.description,
+								updated: j.updated,
+								author: j.author
+							}));
+							return new Response(JSON.stringify(jsonFormatted));
+						});
+					})
+				);
+			} else {
+				fetch(e.request).then((response) => {
+					return cache.add(response);
+				});
+			}
+		});
 	} else {
 		e.respondWith(
-			caches.open('cacheGalerie').then((cache) => cache.match(e.request)).then(function(response) {
+			caches.open('nom_du_cache').then((cache) => cache.match(e.request)).then(function(response) {
 				return response || fetch(e.request);
 			})
 		);
