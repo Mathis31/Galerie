424a680950db8d953ca4ea3b410840a929dc1d20
Mathis31 <52449752+Mathis31@users.noreply.github.com>
2020-11-19 13:49:57 +0100

    commit
    commit

--- sw.js
+++ sw.js
@@ -50,3 +50,13 @@ self.addEventListener('activate', function(e){
     )
 
 })
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+      caches.open('cacheGalerie').then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(['/index.html', '/style.css', '/index.js']);
+      })
+    );
+  });
