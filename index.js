6b084b6de1a86c5130a093c1f9cfa0d51770cdae
Mathis31 <52449752+Mathis31@users.noreply.github.com>
2020-11-19 10:21:41 +0100

    commit
    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register("/sw.js")
+    navigator.serviceWorker.register("sw.js")
     .then((reg) => {
         console.log("Votre service worker a été enregistré!");
     })
