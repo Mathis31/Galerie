ac7c7b20cc31a7f31e2c495f3afe4bd34e79aca2
Mathis31 <52449752+Mathis31@users.noreply.github.com>
2020-11-19 12:23:33 +0100

    commit
    commit

--- sw.js
+++ sw.js
@@ -1,56 +1,38 @@
 self.addEventListener('install', (e) => {
-	e.waitUntil(
-		caches.open('cacheGalerie').then((cache) => {
-			return cache.addAll([ '/index.html', '/style.css', '/index.js' ]);
-		})
-	);
+    e.waitUntil(
+        caches.open('cacheGalerie')
+        .then(cache => {
+            return cache.addAll(['/index.html', '/style.css', '/index.js']);
+        })
+    ); 
 });
 
 self.addEventListener('fetch', (e) => {
-	console.log('[Service Worker] Ressource récupérée ' + e.request.url);
-	if (navigator.onLine) {
-		caches.open('cacheGalerie').then((cache) => {
-			if (e.request.url === 'https://compassionate-lichterman-736604.netlify.app/GalerieRepos/index.json') {
-					const promiseChain = fetch(e.request).then(async (response) => {
-						cache.add(response);
-						return response.json().then((json) => {
-							const jsonFormatted = json.map((j) => ({
-								url: j.url,
-								description: j.description,
-								updated: j.updated,
-								author: j.author
-							}));
-							return new Response(JSON.stringify(jsonFormatted));
-						});
-                    });
-                    
-				    e.respondWith(promiseChain);
-			} else {
-				fetch(e.request).then((response) => {
-					return cache.add(response);
-				});
-			}
-		});
-	} else {
-		e.respondWith(
-			caches.open('cacheGalerie').then((cache) => cache.match(e.request)).then(function(response) {
-				return response || fetch(e.request);
-			})
-		);
-	}
-	/*if(e.request.url === "https://compassionate-lichterman-736604.netlify.app/GalerieRepos/index.json"){
-        e.respondWith(
-            fetch(e.request).then(async (response) => {
-                return response.json().then((json) => {
-                    const jsonFormatted = json.map((j) => ({
-                        url: j.url,
-                        description: j.description,
-                        updated: j.updated,
-                        author: j.author
-                    }))
-                    return new Response(JSON.stringify(jsonFormatted));
+    if(navigator.onLine){
+        console.log('[Service Worker] Ressource récupérée ' + e.request.url);
+        fetch(e.request).then(response=>{return cache.put(e.request,response);}); 
+        if(e.request.url === "https://compassionate-lichterman-736604.netlify.app/GalerieRepos/index.json"){
+            e.respondWith(
+                fetch(e.request).then(async (response) => {
+                    return response.json().then((json) => {
+                        const jsonFormatted = json.map((j) => ({
+                            url: j.url,
+                            description: j.description,
+                            updated: j.updated,
+                            author: j.author
+                        }))
+                        return new Response(JSON.stringify(jsonFormatted));
+                    })
                 })
+            )
+        }
+    }else{
+        e.respondWith(
+            caches.open('cacheGalerie')
+            .then( (cache) => cache.match(e.request) )
+            .then( function(response){
+                return response || fetch(e.request);
             })
-        )
-    }*/
+        ); 
+    }
 });
